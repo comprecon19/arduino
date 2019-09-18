cf794ae98abc57af418375273f591cb6ba446cb6
Stephanie Smith <stephiesmith@gmail.com>
2019-09-18 14:07:07 -0700

    updating code that we did in class

--- In Class/sketch_sep10a/sketch_sep10a.ino
+++ In Class/sketch_sep10a/sketch_sep10a.ino
@@ -1,19 +1,47 @@
 
-int ledPin = 13;
+int ledPin = 11;
+
+int knobPin = A0;
+int knobValue = -1;
+
+int switchPin = 7;
+int switchState = -1;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(ledPin, OUTPUT); 
+  pinMode(switchPin, INPUT);
   // Enable serial communications
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(ledPin, HIGH);
-  Serial.println("Turned LED On");
-  delay(500);
-  digitalWrite(ledPin, LOW);
-  Serial.println("Turned LED Off");
-  delay(500);
+  knobValue = analogRead(knobPin);
+  Serial.print("knobValue = ");
+  Serial.println(knobValue);
+
+  switchState = digitalRead(switchPin);
+  Serial.print("switchState = ");
+  Serial.println(switchState);
+
+  if ( knobValue > 255 ) {
+    // put your main code here, to run repeatedly:
+    digitalWrite(ledPin, HIGH);
+    Serial.println("Turned LED On");
+    delay(knobValue);
+  
+    knobValue = analogRead(knobPin);
+    Serial.print("knobValue = ");
+    Serial.println(knobValue);
+    
+    digitalWrite(ledPin, LOW);
+    Serial.println("Turned LED Off");
+    delay(knobValue);
+  } else if (knobValue >= 0 && knobValue <= 255) {
+    analogWrite(ledPin, knobValue);
+  } else {
+    digitalWrite(ledPin, HIGH);
+  }
+  
+  
 }
